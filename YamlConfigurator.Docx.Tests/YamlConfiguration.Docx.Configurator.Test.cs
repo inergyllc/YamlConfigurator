c4bd4a6820ed02f63fb87c7b9c04994589c01a12
inergyllc <amram.dworkin@inergyllc.com>
2024-07-31 16:39:08 -0400

    YAML and Files tests added.  Saving so Chat can see the code to write all of the tests for DOCX

---
+++ YamlConfigurator.Docx.Tests/YamlConfiguration.Docx.Configurator.Test.cs
@@ -0,0 +1,132 @@
+using Microsoft.Extensions.Configuration;
+using YamlConfigurator.Docx.Models;
+using Microsoft.Extensions.Configuration.Yaml;
+
+
+namespace YamlConfigurator.Tests;
+[TestFixture]
+public class YamlConfigurationTests
+{
+    private IConfiguration _configuration;
+
+    [SetUp]
+    public void Setup()
+    {
+        _configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddYamlFile(
+                "appsettings.yaml" +
+                "",
+                optional: false, reloadOnChange: true)
+            .Build();
+    }
+
+    [Test]
+    public void TestStringValue()
+    {
+        var stringValue = _configuration["stringvalue"];
+        Assert.That(stringValue, Is.EqualTo("teststring"));
+    }
+
+    [Test]
+    public void TestIntValue()
+    {
+        var intValue = _configuration.GetValue<int>("intvalue");
+        Assert.That(intValue, Is.EqualTo(99));
+    }
+
+    [Test]
+    public void TestBoolValue()
+    {
+        var boolValue = _configuration.GetValue<bool>("boolvalue");
+        Assert.That(boolValue, Is.EqualTo(false));
+    }
+
+    [Test]
+    public void TestDoubleValue()
+    {
+        var doubleValue = _configuration.GetValue<double>("doublevalue");
+        Assert.That(doubleValue, Is.EqualTo(122.03));
+    }
+
+    [Test]
+    public void TestDateValue()
+    {
+        var dateValue = _configuration.GetValue<DateTime>("datevalue");
+        Assert.That(dateValue, Is.EqualTo(new DateTime(2024, 1, 1)));
+    }
+
+    [Test]
+    public void TestTimeValue()
+    {
+        var timeValue = _configuration.GetValue<TimeSpan>("timevalue");
+        Assert.That(timeValue, Is.EqualTo(new TimeSpan(14, 33, 4)));
+    }
+
+    [Test]
+    public void TestDateTimeValue()
+    {
+        var dateTimeValue = _configuration.GetValue<DateTime>("datetimevalue");
+        Assert.That(dateTimeValue, Is.EqualTo(new DateTime(2024, 1, 1, 14, 33, 4)));
+    }
+
+    [Test]
+    public void TestValueset()
+    {
+        dynamic valueset = new
+        {
+            stringvalue = _configuration["valueset:stringvalue"],
+            intvalue = _configuration.GetValue<int>("valueset:intvalue"),
+            boolvalue = _configuration.GetValue<bool>("valueset:boolvalue"),
+            doublevalue = _configuration.GetValue<double>("valueset:doublevalue"),
+            datevalue = _configuration.GetValue<DateTime>("valueset:datevalue"),
+            timevalue = _configuration.GetValue<TimeSpan>("valueset:timevalue"),
+            datetimevalue = _configuration.GetValue<DateTime>("valueset:datetimevalue")
+        };
+
+        Assert.AreEqual("teststring", valueset.stringvalue);
+        Assert.AreEqual(99, valueset.intvalue);
+        Assert.AreEqual(false, valueset.boolvalue);
+        Assert.AreEqual(122.03, valueset.doublevalue);
+        Assert.AreEqual(new DateTime(2024, 1, 1), valueset.datevalue);
+        Assert.AreEqual(new TimeSpan(14, 33, 4), valueset.timevalue);
+        Assert.AreEqual(new DateTime(2024, 1, 1, 14, 33, 4), valueset.datetimevalue);
+    }
+
+    [Test]
+    public void TestDocxConfigurationLoading()
+    {
+        var docxConfig = 
+            _configuration.GetSection("docx").Get<DocxConfiguration>()
+            ?? new();
+        var journalDoc = docxConfig.GetDocByName("journal");
+
+        Assert.That(journalDoc, Is.Not.Null);
+        Assert.Multiple(() =>
+        {
+            Assert.That(journalDoc.Name, Is.EqualTo("journal"));
+            Assert.That(journalDoc.TitlePage.Title.Value, Is.EqualTo("LLM Comparison Journal"));
+            Assert.That(journalDoc.TitlePage.Title.Style, Is.EqualTo("Title"));
+        });
+
+        var summaryPart = journalDoc.Parts.Find(part => part.Name == "summary");
+        var qtablePart = journalDoc.Parts.Find(part => part.Name == "qtable");
+
+        Assert.That(summaryPart, Is.Not.Null);
+        Assert.Multiple(() =>
+        {
+            Assert.That(summaryPart.Title.Value, Is.EqualTo("Summary"));
+            Assert.That(summaryPart.Title.Style, Is.EqualTo("Heading 2"));
+            Assert.That(summaryPart.Text.Value, Is.EqualTo("Summarize this LlmComparison run"));
+            Assert.That(summaryPart.Text.Style, Is.EqualTo("Normal"));
+            Assert.That(qtablePart, Is.Not.Null);
+        });
+        Assert.Multiple(() =>
+        {
+            Assert.That(qtablePart.Title.Value, Is.EqualTo("Q-Table - Questions for LLMs"));
+            Assert.That(qtablePart.Title.Style, Is.EqualTo("Heading 2"));
+            Assert.That(qtablePart.Text.Value, Does.Contain("Q-Tables generate formatted questions"));
+            Assert.That(qtablePart.Text.Style, Is.EqualTo("Normal"));
+        });
+    }
+}
---
+++ YamlConfigurator.Tests/YamlConfiguration.Files.Tests.cs
@@ -0,0 +1,187 @@
+﻿using Microsoft.Extensions.Configuration;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.IO;
+using YamlConfigurator.Files.Models;
+using Microsoft.Extensions.Configuration.Yaml;
+
+namespace YamlConfigurator.Files.Tests
+{
+    [TestFixture]
+    public class YamlConfiguratorFilesTests
+    {
+        private IConfiguration _configuration;
+        private FilesManagerConfiguration _filesManagerConfiguration;
+
+        [SetUp]
+        public void Setup()
+        {
+            var basePath = TestContext.CurrentContext.TestDirectory;
+            var yamlFilePath = Path.Combine(basePath, "appsettings.yaml");
+
+            _configuration = new ConfigurationBuilder()
+                .SetBasePath(basePath)
+                .AddYamlFile(yamlFilePath, optional: false, reloadOnChange: true)
+                .Build();
+
+            _filesManagerConfiguration = _configuration.GetFilesManagerConfiguration();
+        }
+
+        [Test]
+        public void TestFilesManagerConfigurationLoaded()
+        {
+            Assert.That(_filesManagerConfiguration, Is.Not.Null);
+            Assert.Multiple(() =>
+            {
+                Assert.That(_filesManagerConfiguration.Folder, Is.EqualTo(".\\"));
+                Assert.That(_filesManagerConfiguration.Timestamp, Is.EqualTo(true));
+                Assert.That(_filesManagerConfiguration.Force, Is.EqualTo(true));
+                Assert.That(_filesManagerConfiguration.Append, Is.EqualTo(false));
+            });
+        }
+
+        [Test]
+        public void TestGetFileByName_Existing()
+        {
+            var fileConfig = _filesManagerConfiguration.GetFileByName("results");
+            Assert.That(fileConfig, Is.Not.Null);
+            Assert.Multiple(() =>
+            {
+                Assert.That(fileConfig.Name, Is.EqualTo("results"));
+                Assert.That(fileConfig.Type, Is.EqualTo("excel"));
+                Assert.That(fileConfig.Root, Is.EqualTo("TabulatedResults"));
+                Assert.That(fileConfig.Extension, Is.EqualTo(".xlsx"));
+            });
+        }
+
+        [Test]
+        public void TestGetFileByName_NonExisting()
+        {
+            var ex = Assert.Throws<KeyNotFoundException>(() => _filesManagerConfiguration.GetFileByName("nonexistent"));
+            Assert.That(ex.Message, Is.EqualTo("File configuration with name 'nonexistent' not found."));
+        }
+
+        [Test]
+        public void TestGetFileByIndex_Existing()
+        {
+            var fileConfig = _filesManagerConfiguration.GetFileByIndex(0);
+            Assert.That(fileConfig, Is.Not.Null);
+            Assert.Multiple(() =>
+            {
+                Assert.That(fileConfig.Name, Is.EqualTo("results"));
+                Assert.That(fileConfig.Type, Is.EqualTo("excel"));
+                Assert.That(fileConfig.Root, Is.EqualTo("TabulatedResults"));
+                Assert.That(fileConfig.Extension, Is.EqualTo(".xlsx"));
+            });
+        }
+
+        [Test]
+        public void TestGetFileByIndex_NonExisting()
+        {
+            var ex = Assert.Throws<IndexOutOfRangeException>(() => _filesManagerConfiguration.GetFileByIndex(10));
+            Assert.That(ex.Message, Is.EqualTo("Index '10' is out of range."));
+        }
+
+        [Test]
+        public void TestIndexerByName_Existing()
+        {
+            var fileConfig = _filesManagerConfiguration["results"];
+            Assert.That(fileConfig, Is.Not.Null);
+            Assert.Multiple(() =>
+            {
+                Assert.That(fileConfig.Name, Is.EqualTo("results"));
+                Assert.That(fileConfig.Type, Is.EqualTo("excel"));
+                Assert.That(fileConfig.Root, Is.EqualTo("TabulatedResults"));
+                Assert.That(fileConfig.Extension, Is.EqualTo(".xlsx"));
+            });
+        }
+
+        [Test]
+        public void TestIndexerByName_NonExisting()
+        {
+            var ex = Assert.Throws<KeyNotFoundException>(() =>
+            {
+                var _ = _filesManagerConfiguration["nonexistent"];
+            });
+            Assert.That(ex.Message, Is.EqualTo("File configuration with name 'nonexistent' not found."));
+        }
+
+
+        [Test]
+        public void TestIndexerByIndex_Existing()
+        {
+            var fileConfig = _filesManagerConfiguration[0];
+            Assert.That(fileConfig, Is.Not.Null);
+            Assert.Multiple(() =>
+            {
+                Assert.That(fileConfig.Name, Is.EqualTo("results"));
+                Assert.That(fileConfig.Type, Is.EqualTo("excel"));
+                Assert.That(fileConfig.Root, Is.EqualTo("TabulatedResults"));
+                Assert.That(fileConfig.Extension, Is.EqualTo(".xlsx"));
+            });
+        }
+
+        [Test]
+        public void TestIndexerByIndex_NonExisting()
+        {
+            var ex = Assert.Throws<IndexOutOfRangeException>(() =>
+            {
+                var _ = _filesManagerConfiguration[10];
+            });
+            Assert.That(ex.Message, Is.EqualTo("Index '10' is out of range."));
+        }
+
+        [Test]
+        public void TestFileConfigurationPath_WithTimestamp()
+        {
+            var fileConfig = _filesManagerConfiguration.GetFileByName("results");
+            var expectedRoot = "TabulatedResults";
+            var expectedExtension = ".xlsx";
+            var path = fileConfig.Path;
+
+            Assert.That(path, Does.Contain(expectedRoot));
+            Assert.That(path, Does.Contain(expectedExtension));
+            Assert.That(path, Does.Contain(DateTime.Now.ToString("yyyyMMddHHmmss")[..12])); // Check timestamp without seconds part
+        }
+
+        [Test]
+        public void TestFileConfigurationPath_WithoutTimestamp()
+        {
+            var fileConfig = _filesManagerConfiguration.GetFileByName("journal");
+            fileConfig.Timestamp = false;
+            var expectedPath = Path.Combine(".\\", "OngoingWorkJournal.docx");
+
+            Assert.That(fileConfig.Path, Is.EqualTo(expectedPath));
+        }
+
+        [Test]
+        public void TestFileConfigurationCondition()
+        {
+            var fileConfig = _filesManagerConfiguration.GetFileByName("results");
+            fileConfig.Condition();
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(Directory.Exists(fileConfig.Folder), Is.True);
+                Assert.That(File.Exists(fileConfig.Path), Is.False);
+            });
+        }
+
+        [Test]
+        public void TestFileConfigurationCondition_FileExists()
+        {
+            var fileConfig = _filesManagerConfiguration.GetFileByName("journal");
+            var path = fileConfig.Path;
+
+            // Create the file to simulate it already exists
+            Directory.CreateDirectory(fileConfig.Folder);
+            File.WriteAllText(path, "dummy content");
+
+            var ex = Assert.Throws<InvalidOperationException>(() => fileConfig.Condition());
+            Assert.That(ex.Message, Is.EqualTo($"File {path} already exists and Force not specified as true"));
+
+            // Cleanup
+            File.Delete(path);
+        }
+    }
+}
